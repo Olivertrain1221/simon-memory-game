4a20c0bf43982fc2e9e2f2c70a6fe512301c32d3
Olivertrain1221 <olivertrain1996@gmail.com>
2022-01-11 23:50:35 +0000

    Add file opening of index to test.js

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,3 +1,10 @@
 /**
  * @jest-environment jsdom
- */                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+ */                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+ 
+ beforeAll(() => {
+     let fs = require("fs");
+     let fileContents = fs.readFileStnc("index.html", "utf-8");
+     document.open();
+     document.write(fileContents);
+ })
