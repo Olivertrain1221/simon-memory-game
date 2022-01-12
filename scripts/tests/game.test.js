81596a722f8845e2b0c930be2a7eb5dab9bc1a96
Olivertrain1221 <olivertrain1996@gmail.com>
2022-01-12 00:11:43 +0000

    Add succesful test for game function of score as a key value

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,16 +1,20 @@
 /**
  * @jest-environment jsdom
- */                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+ */    
+
+const { game } = require("../game");
  
  beforeAll(() => {
      let fs = require("fs");
-     let fileContents = fs.readFileStnc("index.html", "utf-8");
+     let fileContents = fs.readFileSync("index.html", "utf-8");
      document.open();
      document.write(fileContents);
- })
+     document.close();
+ });
 
  describe("game object contains correct keys", () => {
      test("score key exists", () => {
-         except("score" in game).toBe(true);
-     })
- })
+         expect("score" in game).toBe(true);
+     });
+ });
+
